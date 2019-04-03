09e2ff74f8bf184fb2fbd7763c12ec993e8cb944
Nicolaus C <nicolauscg@gmail.com>
2019-04-03 23:00:39 +0700

    refactor course package, add Course class

--- test.py
+++ test.py
@@ -1,28 +1,28 @@
 import unittest
 
 from backtracking import Backtracking
-from course import Course, Meeting, Day, Time
+from course import CourseClass, Meeting, Day, Time
 
 class Test(unittest.TestCase):
   def test_clashing_meetings(self):
-    meetingA = Meeting(Day.MONDAY, Time('08:00'), Time('09:40'))
-    meetingB = Meeting(Day.MONDAY, Time('09:00'), Time('10:40'))
+    meetingA = Meeting(Day.MONDAY, Time('08.00'), Time('09.40'), '3.3113')
+    meetingB = Meeting(Day.MONDAY, Time('09.00'), Time('10.40'), '3.3113')
     self.assertTrue(meetingA.clashWith(meetingB))
     self.assertTrue(meetingB.clashWith(meetingA))
 
-  def test_non_clashing_meetings(self):
-    meetingA = Meeting(Day.MONDAY, Time('08:00'), Time('09:40'))
-    meetingB = Meeting(Day.MONDAY, Time('10:00'), Time('11:40'))
+  def test_non_clashing_meetings_same_day(self):
+    meetingA = Meeting(Day.MONDAY, Time('08.00'), Time('09.40'), '3.3113')
+    meetingB = Meeting(Day.MONDAY, Time('10.00'), Time('11.40'), '3.3113')
     self.assertFalse(meetingA.clashWith(meetingB))
     self.assertFalse(meetingB.clashWith(meetingA))
     
-  def test_non_clashing_meetings_2(self):
-    meetingC = Meeting(Day.MONDAY, Time('08:00'), Time('08:50'))
-    meetingD = Meeting(Day.THURSDAY, Time('08:00'), Time('09:40'))
+  def test_non_clashing_meetings_different_day(self):
+    meetingC = Meeting(Day.MONDAY, Time('08.00'), Time('08.50'), '3.3113')
+    meetingD = Meeting(Day.THURSDAY, Time('08.00'), Time('09.40'), '3.3113')
     self.assertFalse(meetingC.clashWith(meetingD))
     self.assertFalse(meetingD.clashWith(meetingC))
 
-  def test_backtracking_with_solution(self):
+  def test_simple_backtracking_with_solution(self):
     bt = Backtracking()
     bt.add_variable('a', [1,2,3])
     bt.add_variable('b', [2,3])
@@ -30,7 +30,7 @@ class Test(unittest.TestCase):
     bt.add_binary_constraint_to_all(lambda x, y: x != y)
     self.assertEqual(bt.solve(), {'a':1, 'b':2, 'c':3}.items())
   
-  def test_backtracking_without_solution(self):
+  def test_simple_backtracking_without_solution(self):
     bt = Backtracking()
     bt.add_variable('a', [1,2])
     bt.add_variable('b', [1,2])
