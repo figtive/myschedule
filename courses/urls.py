d59d4044e5bebd0f1ffef8396d722562a7b54417
Nicolaus C <nicolauscg@gmail.com>
2019-04-08 23:23:35 +0700

    add unimplemented filldb view function, restricted to superuser

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 app_name = 'courses'
 urlpatterns = [
+  path('filldb/', views.fill_course_data_to_db, name='fill_course_data_to_db')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 
 def home_index(request):
   return render(request, 'home/index.html')
+
+def fill_course_data_to_db(request):
+  if request.user.is_superuser:
+    return HttpResponse('you are a super user!')
+  else:
+    return HttpResponseForbidden()
