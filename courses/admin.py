c8959842a96c226612dd0f3c7f3fcfe049ecbac7
Nicolaus C <nicolauscg@gmail.com>
2019-04-08 22:39:37 +0700

    register models to show in admin page

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, CourseClass, Lecturer, Meeting
+
+admin.site.register(Course)
+admin.site.register(CourseClass)
+admin.site.register(Lecturer)
+admin.site.register(Meeting)
