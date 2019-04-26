8f895a19271d287667d6b716638aacf6d292d191
Nicolaus C <nicolauscg@gmail.com>
2019-04-26 07:44:04 +0700

    add success fail notification after submit

--- static/js/form.js
+++ static/js/form.js
@@ -78,10 +78,12 @@ $(document).ready(function() {
       },
       success: function(result){
         if (!result.data.solution_found) {
-          $('.modal#fail').addClass('is-active')
+            // $( ".alert-box.failure" ).fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+          notifyFailure()
           return
         }
-        $('.modal#success').addClass('is-active')
+        // $('.alert-box.success').fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+        notifySuccess()
         addEventsToCalendar(result)
         var contentAdded = '';
         var i, courseInfo, classInfo;
@@ -120,4 +122,45 @@ $(document).ready(function() {
 
 function clearSelectedClass() {
   $(".selected-classes").html('')
+}
+
+function notifySuccess() {
+  const successAlertBox = $(`
+    <div class="alert-box success">
+      <div style="display: flex;">
+        <div style="height: 50px; width: 50px; display: inline-block">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+            <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
+            <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
+          </svg>
+        </div>
+        <span class="text">Successful Alert !!!</span>
+      </div>
+    </div>
+  `)
+  $(".alerts").prepend(successAlertBox);
+  successAlertBox.fadeIn( 300 ).delay( 1500 ).fadeOut( 400, function() {
+    $(this).remove();
+  })
+}
+
+function notifyFailure() {
+  const failureAlertBox = $(`
+    <div class="alert-box failure">
+      <div style="display: flex;">
+        <div style="height: 50px; width: 50px; display: inline-block">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+            <circle class="path circle" fill="none" stroke="#D06079" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
+            <line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"/>
+            <line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"/>
+          </svg>
+        </div>
+        <span class="text">Failure Alert !!!</span>
+      </div>
+    </div>
+  `)
+  $(".alerts").prepend(failureAlertBox);
+  failureAlertBox.fadeIn( 300 ).delay( 1500 ).fadeOut( 400, function() {
+    $(this).remove();
+  })
 }
